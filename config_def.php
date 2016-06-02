f8c968664b424a64253e62fccba4cf22fecf7e99
andoniaf <andonialonsof@gmail.com>
2016-06-02 19:18:02 +0200

    Add config_def.php

---
+++ config_def.php
@@ -0,0 +1,15 @@
+<?php
+
+// parámetros usado para conectar a la base de datos
+$host = "localhost";
+$db_name = "SnortRulez";
+$username = "root";
+$password = "";
+
+//
+$intentos_login = 5;
+$tiempo_fuerzabruta = 2; //horas
+$secure = false; //por defecto no obligar a https
+
+//
+$default_action ='instrucciones';
